a2076f2d967d066df5a8b76e08174bf94ee96d5d
hawi19952 <hussain@mohtwize.com>
2021-11-29 21:59:25 +0300

    running an http request with .env

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http'); 
+const dotenv = require('dotenv').config(); 
+
+const port = process.env.PORT || 3000; 
+
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200; 
+    res.setHeader('content-type', 'text/html'); 
+    res.end('<h1> Hello Word <h1/>');
+}); 
+
+const logServerStatus = (port) => {
+    console.log(`Server running at port ${port}`);
+}
+server.listen(port, logServerStatus(port)); 
