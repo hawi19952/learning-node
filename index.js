ed032232336e1f267e5614f2b0caff05ee81f59e
hawi19952 <hussain@mohtwize.com>
2021-11-30 15:10:37 +0300

    playing with fs and building objects with it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http'); 
 const dotenv = require('dotenv').config(); 
+const fs = require('fs');
 
 const port = process.env.PORT || 3000; 
 
@@ -14,3 +15,34 @@ const logServerStatus = (port) => {
     console.log(`Server running at port ${port}`);
 }
 server.listen(port, logServerStatus(port)); 
+
+
+// FS changes here! 
+/*try {
+    fs.renameSync('after.json', 'before.json'); 
+} catch (err) {
+    console.error(err);
+}
+*/
+const presentName = (jsonObject) => {
+    let outputValue = jsonObject.name; 
+    return outputValue; 
+}
+
+const jsonParser =  (fileReader) => {
+    let dataObject = JSON.parse(fs.readFileSync(fileReader)); 
+
+    return dataObject; 
+}
+
+const objectifyJson = (fileName, propertyName) => {
+    let objectOutput = jsonParser(fileName);
+    let keyValue = objectOutput.propertyName; 
+}
+
+try {
+    presentName(objectifyJson('before.json'));
+    
+} catch (error) {
+    console.error(error);
+}
