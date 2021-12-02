0814aaab1fec703687aac1a3093cc86724efd35b
hawi19952 <hussain@mohtwize.com>
2021-12-02 21:23:58 +0300

    setting up the server and the environment with dotenv

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require('http'); 
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv').config(); 
 
 const port = process.env.PORT || 3000; 
 const env = process.env.NODE_ENV || "test"; 
 
 
 
-const server = http.createServer()
+const server = http.createServer(
+    (req,res) => {
+        res.statusCode = 200; 
+        res.setHeader = ('content-type', 'text/html'); 
+        res.end('<h1> Hello World <h1/>');
+    }
+);
+
+
+server.listen(port, console.log(`Server is running ${env} in port ${port}`))
