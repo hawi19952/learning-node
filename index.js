d10e27c533df41a06de60a734ba1946683fa1d1c
hawi19952 <hussain@mohtwize.com>
2021-12-02 23:36:25 +0300

    Doing everything

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 const http = require('http'); 
 const dotenv = require('dotenv').config(); 
+const fs = require('fs');
 
 const port = process.env.PORT || 3000; 
 const env = process.env.NODE_ENV || "test"; 
-
-
+const hostname = process.env.HOSTNAME || "host";
 
 const server = http.createServer(
     (req,res) => {
-        res.statusCode = 200; 
-        res.setHeader = ('content-type', 'text/html'); 
-        res.end('<h1> Hello World <h1/>');
+        let htmlFile = '';
+        console.log(`file Now is ${htmlFile}`);
+        switch(req.url) {
+            case '/':
+                htmlFile = 'index.html'; 
+                break;
+            case '/home':
+                    htmlFile = 'index.html'; 
+                    break;
+            case '/about': 
+                htmlFile = 'about.html'; 
+                break;
+            case '/contact-me': 
+                htmlFile = 'contact-me.html'; 
+                break;
+            
+            default:
+                htmlFile = '404.html';
+                break;
+        }
+        if(htmlFile){
+            console.log(`processing file ${htmlFile} `);
+            renderPage(res, htmlFile); 
+        }
     }
 );
+
+function renderPage(res, htmlFile) {
+    const filePath = `./${htmlFile}`; 
+    fs.stat(filePath, (stats, err) => {
+        res.statusCode = 200; 
+        res.setHeader('content-type', 'text/html'); 
+        fs.createReadStream(htmlFile).pipe(res);
+    })
+}
 
 
 server.listen(port, console.log(`Server is running ${env} in port ${port}`))
