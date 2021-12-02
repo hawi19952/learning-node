12fbcfad9ec28e2ceb12815d0dacf7106d6a6f68
hawi19952 <hussain@mohtwize.com>
2021-12-02 19:52:22 +0300

    initiating the project again

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const http = require('http'); 
-const dotenv = require('dotenv').config(); 
+const dotenv = require('dotenv'); 
 
 const port = process.env.PORT || 3000; 
+const env = process.env.NODE_ENV || "test"; 
 
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200; 
-    res.setHeader('content-type', 'text/html'); 
-    res.end('<h1> Hello Word <h1/>');
-}); 
 
-const logServerStatus = (port) => {
-    console.log(`Server running at port ${port}`);
-}
-server.listen(port, logServerStatus(port)); 
+const server = http.createServer()
